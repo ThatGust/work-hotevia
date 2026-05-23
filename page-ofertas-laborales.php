d0db519f26d4cb30d45b7f645ad3e72b83b5523d
gcano <guillermo.cano.luque@gmail.com>
2026-05-22 21:54:42 -0500

    bug fixes

--- page-ofertas-laborales.php
+++ page-ofertas-laborales.php
@@ -265,7 +265,8 @@
                                           <span class="icon"><?php echo $svg_icon; ?></span>
 
                                           <?php if ($sf_title): ?>
-                                             <span class="job-title-list"><?php echo $sf_title; ?> - </span>
+                                             <span class="job-title-list"><?php echo $sf_title; ?></span>
+                                             <span class="job-separator"> - </span>
                                           <?php endif; ?>
 
                                           <?php if ($sf_empresa): ?>
